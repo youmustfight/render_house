03f68211264724eda9c794176c8a6fa3d45aa917
noelleantoinette <noelleantoinette@gmail.com>
2015-08-22 17:04:33 -0400

    no really stripe works

--- server/app/routes/payment/payment.router.js
+++ server/app/routes/payment/payment.router.js
@@ -3,14 +3,14 @@ var router = require('express').Router();
 module.exports = router;
 var mongoose = require('mongoose');
 
-var stripe = require("stripe")("sk_test_chgWT2wV5cj1gxZsNqQTSaLb");
+var stripe = require("stripe")("sk_test_WhzuBvvju6AKl7KyIKtdWiQf");
 
 router.post('/', function(req, res, next) {
     stripe.charges.create({
         amount: req.body.amount,
         currency: "usd",
         source: req.body.token,
-        description: req.body.manifest
+        description: ""
     }, function(err, charge) {
         res.send('Charged! Details: ' + charge);
     });
